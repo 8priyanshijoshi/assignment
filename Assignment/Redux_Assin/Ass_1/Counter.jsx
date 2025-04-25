1b707c3471706c3ee03678aa5f74536b1713ffb6
8priyanshijoshi <piyujoshi157@gmail.com>
2025-04-25 16:54:33 +0530

    Add files via upload

---
+++ Assignment/Redux_Assin/Ass_1/Counter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
+
+const Counter = () => {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
